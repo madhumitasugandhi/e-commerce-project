320f22ddbfee1517cb6b0a6d98d3a6dc3aa4ec0d
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-01 13:11:47 +0530

    Order model Schema created.

---
+++ server/models/Order.js
@@ -0,0 +1,70 @@
+import { model, Schema } from 'mongoose';
+
+const orderSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    products: [
+        {
+        productId: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    },
+    ],
+    totalBills: {
+        type: Number,
+        required: true,
+    },
+    deliveryAddress: {
+        type: String,
+        required: true,
+    },
+    phone: {
+            type: String,
+            required: true,
+        },
+    paymentMode: {
+        type: String,
+        required:true,
+    },
+    paymentId: {
+        type: Schema.Types.ObjectId,
+        ref: "Payment",
+        required:true,
+    },
+    status: {
+        type: String,
+        default: "pending",
+    },
+    timeline: [
+        {
+            status:{
+                type : String,
+                required: true,
+            },
+            date: {
+                type: Date,
+                default: Date.now,
+            },
+        },
+    ],
+},
+    {
+        timestamps: true
+    })
+
+const Order = model("Order", orderSchemaSchema);
+
+export default Order;
