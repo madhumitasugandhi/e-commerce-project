f255b6700259e1be4f408a4b5e7ef8f1168e031b
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-01 19:58:07 +0530

    Signup API created in Index.js file.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import bcrypt from 'bcrypt';
+
+import User from './models/User.js';
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 
@@ -25,6 +29,73 @@ app.get("/health", (req, res)=>{
     })
 })
 
+//API for E-commerce application
+app.post("/signup", async(req, res)=>{
+    const{name, email, phone, address, password, rePassword} = req.body;
+
+    if(!password){
+        return res.status(400).json({
+            success: false,
+            message: "Password is required"
+        });
+    }
+    if(password !== rePassword){
+        return res.status(400).json({
+            success: false,
+            message: "Password dose not match"
+        });
+    }
+    if(!name){
+        return res.status(400).json({
+            success: false,
+            message: "Name is required"
+        });
+    }
+    if(!email){
+        return res.status(400).json({
+            success: false,
+            message: "Email is required"
+        });
+    }
+    if(!phone){
+        return res.status(400).json({
+            success: false,
+            message: "Contact number is required"
+        });
+    }
+    if(!address){
+        return res.status(400).json({
+            success: false,
+            message: "Address is required"
+        });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+
+    try{
+    const newUser = new User({
+        name,
+        email,
+        phone,
+        address,
+        password: bcrypt.hashSync(password, salt),
+    });
+
+    const savedUser = await newUser.save();
+
+    return res.status(200).json({
+        success: true,
+        message: "Signup Successful",
+        data:savedUser,
+    });
+}
+catch(error){
+    return res.status(400).json({
+        success: false,
+        message: error.message
+    });
+}
+}) 
 
 //page not found API
 app.use((req, res) => {
