df274c744a36679724c5bfb3ee0b40042def9853
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-07 09:51:27 +0530

    Utility function created.

---
+++ client/src/utils/comman.jsx
@@ -0,0 +1,33 @@
+import { set } from "mongoose";
+
+const getCurrentUser = () => {
+    const user = localStorage.getItem("e-commerce-user-token-details");
+    if(!user) {
+        return null;
+    }
+
+    try {
+        return JSON.parse(user);
+    } catch (error) {
+        console.error("Error parsing user data:", error);
+        return null;
+    }
+};
+
+const getJwtToken = () => {
+    const token = localStorage.getItem("e-commerce-user-token");
+    if(!token) {
+        return null;
+    }
+
+    return `Bearer ${token}`;
+};
+
+const logout = () => {
+    localStorage.clear();
+    setTimeout(() => {
+        window.location.href = "/login";
+    }, 1000);
+};
+
+export { getCurrentUser, getJwtToken, logout };
