c13231d259fdd07a3bdbfb901d3e67fe6c685b04
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-07-02 11:44:49 +0530

    Hero is responsive for sm, md, and lg devices.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -12,24 +12,27 @@ const Hero = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % carouselImages.length);
-    }, 5000); 
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="w-full flex justify-center relative my-6">
-      <img
-        src={carouselImages[index]}
-        alt={`UrbanKart Banner ${index + 1}`}
-        className="w-[80%] max-h-[500px] rounded-2xl shadow-xl object-cover transition-all duration-700 ease-in-out"
-      />
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[80%] h-full flex items-center justify-center">
-        <div className="backdrop-blur-md bg-black/30 p-6 rounded-xl w-[95%] h-[90%] text-center text-white">
-          <p className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white leading-relaxed">
-            Discover timeless comfort and modern elegance, all under one roof.<br />
-            From chic cushions to cozy rugs — <strong>UrbanKart</strong> transforms your space.
-          </p>
+    <div className="w-full px-2 sm:px-4 md:px-6 lg:px-10 xl:px-20 relative my-6">
+      <div className="relative w-full rounded-2xl overflow-hidden">
+        <img
+          src={carouselImages[index]}
+          alt={`UrbanKart Banner ${index + 1}`}
+          className="w-full h-[220px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover rounded-2xl shadow-xl transition-all duration-700 ease-in-out"
+        />
+        <div className="absolute inset-0 flex items-center justify-center px-2 sm:px-4 md:px-10 lg:px-20">
+          <div className="backdrop-blur-md bg-black/40 px-4 py-6 sm:px-10 sm:py-10 md:px-12 md:py-12 lg:px-30  lg:py-24 rounded-xl text-center text-white max-w-4xl w-full">
+            <p className="text-sm sm:text-base md:text-xl lg:text-3xl font-semibold leading-snug sm:leading-normal lg:leading-relaxed">
+              Discover timeless comfort and modern elegance, all under one roof.<br />
+              From chic cushions to cozy rugs — <strong>UrbanKart</strong> transforms your space.
+            </p>
+          </div>
         </div>
+
       </div>
     </div>
   );
