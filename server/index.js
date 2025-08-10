8d85e3dc32a2f68cc5d0fee1a326b350ecce1f43
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-08-10 19:07:13 +0530

    origin path change.

--- server/index.js
+++ server/index.js
@@ -29,8 +29,8 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-    origin: "http://localhost:5173",
-    credentials: true,
+    //origin: "http://localhost:5173",
+    //credentials: true,
     }
 ));
 app.use(session({secret: "test session", cookie:{maxAge:1000 * 60 * 60, httpOnly: false, secure: false}, resave : true,  saveUninitialized : true} ));
